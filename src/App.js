e6e4789c46e9e961e67c07f0745471256581bd53
PTIT-NoTarget <HanND.B21CN324@stu.ptit.edu.vn>
2024-05-15 04:57:24 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router} from "react-router-dom";
+
+import AppLayout from "./components/AppLayout";
+
+const App = (props) => {
+  return (
+
+    <Router>
+      <AppLayout />
+    </Router>
+  );
+}
+
+export default App;
